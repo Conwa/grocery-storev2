01c38df3fb2bcd4a26efb937128518ac862ef46b
Conwa <conradodelcarlo97@gmail.com>
2023-06-13 17:22:56 -0300

    continue learning

---
+++ src/pages/posts/index.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const PostList = ({ posts }) => {
+  return (
+    <>
+      <h1>List of Posts</h1>
+      {posts.map((el) => {
+        return (
+          <div key={el.id}>
+            {" "}
+            <Link href={`/posts/${el.id}`} passHref>
+              {" "}
+              <h2>
+                {el.id} {el.title}
+              </h2>
+            </Link>
+            <hr></hr>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  return { props: { posts: data.slice(0, 3) } };
+}
+
+export default PostList;
