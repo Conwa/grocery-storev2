b5488bfa473abce768a23f131acd7977ff56926f
Conwa <conradodelcarlo97@gmail.com>
2023-06-14 18:54:46 -0300

    continue learning

--- src/pages/posts/[postId].js
+++ src/pages/posts/[postId].js
@@ -20,3 +20,37 @@ export async function getStaticProps(context) {
   const data = await response.json();
   return { props: { post: data } };
 }
+
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  const paths = data.map((post) => {
+    return {
+      params: {
+        postId: `${post.id}`,
+      },
+    };
+  });
+  return {
+    paths: paths,
+    // paths: [
+    //   {
+    //     params: { postId: "1" },
+    //   },
+    //   {
+    //     params: { postId: "2" },
+    //   },
+    //   {
+    //     params: { postId: "3" },
+    //   },
+    //   {
+    //     params: { postId: "4" },
+    //   },
+    //   {
+    //     params: { postId: "5" },
+    //   },
+    // ],
+    fallback: false,
+  };
+}
--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -26,7 +26,7 @@ export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await response.json();
 
-  return { props: { posts: data.slice(0, 3) } };
+  return { props: { posts: data } };
 }
 
 export default PostList;
